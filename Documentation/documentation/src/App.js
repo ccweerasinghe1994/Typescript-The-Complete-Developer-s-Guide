1e18dbc11a2e7b2096bf70dc51aeba78147fc9b4
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-09-16 18:18:16 +0530

    adding dependancies :tea:

--- Documentation/documentation/src/App.js
+++ Documentation/documentation/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core';
+import Header from './components/Header/Header.Component';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './Pages/HomePage/HomePage.Component';
+import SignInAndSignUpPage from './Pages/SignUpAndSignUpPage/SignUpAndSignUpPage.Component';
 
-function App() {
+const useStyles = makeStyles((theme) => {
+  return {
+    root: {
+      flexGrow: 1,
+    },
+  };
+});
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Header />
+      <div>
+        <Switch>
+          <Route exact path='/login' component={SignInAndSignUpPage} />
+          <Route exact path='/' component={HomePage} />
+        </Switch>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ Documentation/documentation/src/Pages/SignUpAndSignUpPage/SignUpAndSignUpPage.Component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignInAndSignUpPage = () => {
+  return <div>Sign In And SignUp Page</div>;
+};
+
+export default SignInAndSignUpPage;
